4f30f723735262d7770ef82421b3ed92d670ee98
FifaOlivia <felicite.agoinon@epitech.eu>
2024-08-16 09:49:50 +0100

    delete about file

--- To_Do2_project/src/stores/counter.js
+++ To_Do2_project/src/stores/counter.js
@@ -1,27 +1,27 @@
 //import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import router from '@/router'
-//import { list } from 'postcss'
+//import { list } from 'postcss' 
 
 export const useCounterStore = defineStore('Counter', {
-  state: () => ({
+  state: () => ({ 
     allList: []
-  }),
+  }), 
   actions: {
     save(list) {
-      this.allList.push(list)
+      this.allList.push(list)  
       localStorage.setItem('lists', JSON.stringify(this.allList))
       router.push('/backlist')
       alert('To do create successfully')
-    },
+    },  
     GetList() {
       this.allList = JSON.parse(localStorage.getItem('lists'))
-    },
+    }, 
     UpdateList(listId) {
       this.allList[this.allList.indexOf(this.allList.find((list) => list.id == listId.id))] = listId
       localStorage.setItem('lists', JSON.stringify(this.allList))
       alert('To do updated successfully')
-    },
+    }, 
     DeleteList(listId) {
       this.allList = this.allList.filter((list) => list.id !== listId)
       localStorage.setItem('lists', JSON.stringify(this.allList))
@@ -29,6 +29,6 @@ export const useCounterStore = defineStore('Counter', {
       alert('To do deleted successfully')
     }
   }
-})
+})  
 
 export default useCounterStore
